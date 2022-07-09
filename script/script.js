e84098e672d7cbc3ebf45b650eecaf7e7f594098
hbin2 <andydtngpqls@naver.com>
2022-07-09 23:19:59 +0900

    메세지

--- script/script.js
+++ script/script.js
@@ -1,8 +1,27 @@
 new fullpage('#fullpage', {
     licenseKey: 'YOUR KEY HERE',
-    sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
+    
   });
 
+  // aos 라이브러리
 
   AOS.init();
+
+  // 서브타이틀 애니메이션
+  gsap.from(".sub-title > span > span", {
+    opacity: 0,
+    y: 100,
+    duration: 1,
+    stagger: 0.8
+  });
+
+  gsap.to(".sub-title > span > span", {
+    opacity: 0,
+    y: -100,
+    duration: 1,
+    stagger: {
+      each: "0.1",
+      from: "end"
+    }
+  }, "+=2");
   
