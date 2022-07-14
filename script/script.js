cc5d07e1dc596524c9d3d3c72472b0d3df3dff07
hbin2 <andydtngpqls@naver.com>
2022-07-14 20:50:42 +0900

    메세지

--- script/script.js
+++ script/script.js
@@ -3,7 +3,11 @@ new fullpage('#fullpage', {
     navigation:true,
     navigationPosition: 'right',
     showActiveTooltip:true,
-
+    responsiveWidth:1900,
+    scrollOverflow: true,
+    afterResponsive: function(isResponsive){
+      //alert("Is responsive: " + isResponsive);
+    }
    
   });
 
